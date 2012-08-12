7b26cd1e1d7ac10c04f822586bd8e485a9cd62a3
Ethan Jucovy <ethan.jucovy@gmail.com>
2012-08-12 04:43:40 -0400

    fix nasty bug in config reader, and implement a config writer

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from StringIO import StringIO
+
 def read_config(fp):
     repos = dict()
     this_repo = None
@@ -17,10 +19,55 @@ def read_config(fp):
                 if perm in info:
                     info[perm].extend(users)
                 else:
-                    info[perm] = users
+                    info[perm] = list(users) ## Copy it!
 
     if this_repo is not None and len(info) > 0:
         repos[this_repo] = info
 
     fp.close()
     return repos
+
+
+def to_string(config):
+
+    def _sort(perms):
+        tail = []
+        ## Ensure the + goes last
+        if '+' in perms:
+            perms = perms.replace("+", '')
+            tail.append("+")
+        perms = sorted(perms)
+        perms.extend(tail)
+        return ''.join(perms)
+
+    fp = StringIO()
+    for repo in sorted(config):
+        fp.write("repo\t%s\n" % repo)
+
+        ## Combine permissions
+        perms = config[repo]
+        transposed = {}
+        for perm in perms:
+            for user in perms[perm]:
+                if user in transposed:
+                    transposed[user] += perm
+                    transposed[user] = _sort(transposed[user])
+                else:
+                    transposed[user] = perm
+
+
+        detransposed = {}
+        for user in transposed:
+            if transposed[user] in detransposed:
+                detransposed[transposed[user]].append(user)
+                detransposed[transposed[user]] = sorted(detransposed[transposed[user]])
+            else:
+                detransposed[transposed[user]] = [user]
+
+        for permset in sorted(detransposed):
+            fp.write("\t%s\t=\t%s\n" % (permset, ' '.join(detransposed[permset])))
+
+        fp.write("\n")
+    fp.seek(0)
+    return fp.read()
+
